8e8386b145a020ba69604059bc33030b3bd28b03
Kaushik Burkule <14887493+kaushikburkule@users.noreply.github.com>
2021-12-04 18:18:30 +0530

    Update UserDao.kt

--- app/src/main/java/com/system/assignment/data/local/dao/UserDao.kt
+++ app/src/main/java/com/system/assignment/data/local/dao/UserDao.kt
@@ -1,6 +1,8 @@
 package com.system.assignment.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.system.assignment.data.local.entity.User
 
@@ -9,5 +11,14 @@ interface UserDao {
 
     @Query("SELECT * FROM user")
     fun loadAll(): List<User?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User?)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(users: List<User?>?)
+
+    @Query("SELECT * FROM user WHERE id IN (:userIds)")
+    fun loadAllByIds(userIds: List<Int?>?): List<User?>?
 
 }
